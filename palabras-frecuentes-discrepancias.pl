69bdf56726dd68f740d4a3e0628d454ad1b64002
rbnfr <ruben.f.alcaraz@gmail.com>
2020-04-13 15:45:51 +0200

    Small update
    Updated gitignore, fasta file, path to module

--- palabras-frecuentes-discrepancias.pl
+++ palabras-frecuentes-discrepancias.pl
@@ -1,8 +1,12 @@
 $|=1;
 use strict;
 use warnings;
+use lib 'C:\Users\rfrancoa\Documents\Repositories\Frequent-words';
 use bio;
+
 my $secuencia = "ACGTTGCATGTCGCATGATGCATGAGAGCT";
 my $k = 5;
 my $discrepancias = 2;
-print "En la siguiente secuencia: ".$secuencia.", se repiten con más frecuencia los siguientes grupos de ".$k." nucleotidos si se permiten ".$discrepancias." discrepancias: ".join(", ",palabrasfrecuentesdiscrepancias($secuencia,$k,$discrepancias)).".";
+
+print "En la siguiente secuencia: ".$secuencia.", se repiten con más frecuencia los siguientes grupos de ".$k." nucleotidos si se permiten ".$discrepancias." discrepancias: \n";
+print join("\n",palabrasfrecuentesdiscrepancias($secuencia,$k,$discrepancias));
